036d32bd21158f35e42948fa5f38d0ec4ad19e55
Shivesh Tiwari <algomonk016@gmail.com>
2021-12-08 00:46:43 +0530

    added reducer function

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ function buyCake() {
         info: 'First Redux Action'
     }
 }
+
+const initialState = {
+    numberOfCakes: 10
+} 
+
+const reducer = (state, action) => {
+    switch(action.type){
+        case BUY_CAKE:
+            return {
+                ...state,
+                numberOfCakes: state.numberOfCakes - 1
+            }
+        default: state
+    }
+}
