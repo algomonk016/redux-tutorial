5182b1d6c5d3491ed540f448a84a472e8e3d1afa
Shivesh Tiwari <algomonk016@gmail.com>
2021-12-08 00:01:22 +0530

    added action

--- index.js
+++ index.js
@@ -6,15 +6,3 @@ function buyCake() {
         info: 'First Redux Action'
     }
 }
-
-
-/*
-    action creater:
-        - a function that returns action
-    
-    action:
-        - plain js object
-        - having a type property
-        - only way our application can interact with the store
-        - carry some information from our app to redux store
-*/ 
