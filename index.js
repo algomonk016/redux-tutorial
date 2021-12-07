6005d93d5a6b45c0001601c0731c81c0a0d9942f
Shivesh Tiwari <algomonk016@gmail.com>
2021-12-07 23:56:45 +0530

    created an action

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('from index.js')
+const BUY_CAKE = 'BUY_CAKE'
+
+function buyCake() {
+    return {
+        type: BUY_CAKE,
+        info: 'First Redux Action'
+    }
+}
+
+
+/*
+    action creater:
+        - a function that returns action
+    
+    action:
+        - plain js object
+        - having a type property
+        - only way our application can interact with the store
+        - carry some information from our app to redux store
+*/ 
